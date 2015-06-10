2605c3e0727b3a02e671c69f1618c360d786a77c
Andy Sowar <andy.sowar@gmail.com>
2015-06-09 18:29:59 -0600

    Add Chapter 7 homework
    This includes ex7.rb and Readme.md
    
    [#95933838]

---
+++ chapter_7/Readme.md
@@ -0,0 +1,6 @@
+#Difference between `print` & `puts`.
+
+>`print` & `puts` both display the results of whatever code
+>is comes after it. However, puts will add a new line after
+>executing & print will not.
+>That is why the two lines in the example end up together.
---
+++ chapter_7/ex7.rb
@@ -0,0 +1,41 @@
+# This is a string.
+puts 'Mary had a little lamb.'
+# This is a string with a string inside of it.
+puts "Its fleece was white as #{'snow'}."
+# This is another string.
+puts 'And everywhere that Mary went.'
+# This takes the . and prints 10 of them.
+puts '.' * 10 # what'd that do?
+
+# Variable end1 is equal to the string 'C'
+end1 = 'C'
+# Variable end2 is equal to the string 'h'
+end2 = 'h'
+# Variable end3 is equal to the string 'e'
+end3 = 'e'
+# Variable end4 is equal to the string 'e'
+end4 = 'e'
+# Variable end5 is equal to the string 's'
+end5 = 's'
+# Variable end6 is equal to the string 'e'
+end6 = 'e'
+# Variable end7 is equal to the string 'B'
+end7 = 'B'
+# Variable end8 is equal to the string 'u'
+end8 = 'u'
+# Variable end9 is equal to the string 'r'
+end9 = 'r'
+# Variable end10 is equal to the string 'g'
+end10 = 'g'
+# Variable end11 is equal to the string 'e'
+end11 = 'e'
+# Variable end12 is equal to the string 'r'
+end12 = 'r'
+
+# watch that print vs. puts on this line what's it do?
+# print & puts both display the results of whatever code
+# is comes after it. However, puts will add a new line after
+# executing & print will not.
+# That is why these two lines end up together.
+print end1 + end2 + end3 + end4 + end5 + end6
+puts end7 + end8 + end9 + end10 + end11 +end12
