9d21fe8fd56843cd1700359da79c19533d4ac4e5
Andy Sowar <andy.sowar@gmail.com>
2015-06-14 10:44:52 -0600

    Add chapter 10 homework
    [#95933844]

---
+++ chapter_10/Readme.md
@@ -0,0 +1,8 @@
+#Study Drills
+
+######Use ''' (triple-single-quote) instead. Can you see why you might use that instead of """?
+>Using triple-double-quotes gets rid of the escape characters. Using the triple-single-quotes
+>shows all of the escape characters. You might use that when showing an example where you want escape
+>characters to be visible.
+
+
---
+++ chapter_10/ex10.rb
@@ -0,0 +1,23 @@
+tabby_cat = "\tI'm tabbed in."
+persian_cat = "I'm split\non a line."
+backslash_cat = "I'm \\ a \\ cat."
+
+fat_cat = """
+I'll do a list:
+\t* Cat food
+\t* Fishes
+\t* Catnip\n\t* Grass
+"""
+
+smelly_cat = """
+\t\t• Smelly cat
+\t\t\t• Smelly cat
+\t\t\t\t• What are they feeding you?
+\t\t\t\t\t\t• It's ok to be smelly
+"""
+
+puts tabby_cat
+puts persian_cat
+puts backslash_cat
+puts fat_cat
+puts smelly_cat
